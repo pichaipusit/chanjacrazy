3395c3c4281702fe266ddf397cef49521339ff11
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2025-06-28 01:01:50 +0700

    fix: ui container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import SlotMachine from "@/components/SlotMachine";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen container">
+    <div className="min-h-screen">
       <SlotMachine />
     </div>
   );
